984e4d8a05e2ccfb9bc9ae4c5e1b88ceae7efee6
08ppty <pemmarajupavan08@gmail.com>
2023-11-30 10:00:47 +0530

    Commitig changes  for starhealth assignment using testng

---
+++ AssignmentTestNG/src/test/java/StarHealthPage.java
@@ -0,0 +1,19 @@
+ 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class StarHealthPage {
+
+	
+	
+	@Test
+	public void print_link()
+	{
+		WebDriver driver = new ChromeDriver();
+		
+	String link1=	driver.findElement(By.linkText("Glossary")).getText();
+	
+	}
+}
